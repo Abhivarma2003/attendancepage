8887ca61133ad4a9a5170f4f244b438a53d49e9e
Abhivarma2003 <kabhishekvarma908@gmail.com>
2025-01-07 22:13:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AttendancePage from './components/AttendancePage';
+
+function App() {
+  return (
+    <div>
+      <AttendancePage />
+    </div>
+  );
+}
+
+export default App;
